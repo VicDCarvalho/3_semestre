35c791a4a371b00263b1b09f5136960f95c4ac95
VicDCarvalho <jaquelinecarvalho1972@gmail.com>
2026-09-10 16:46:48 -0300

    Aula 4 de Lab

---
+++ ED/instagram.c
@@ -0,0 +1,240 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct vizinho {
+    char nome[10];
+    struct vizinho *prox;
+} TVizinho;
+
+typedef struct grafo {
+    char nome[10];
+    int idade;
+    TVizinho *prim_vizinho;
+    struct grafo *prox;
+} TGrafo;
+
+TGrafo *insere_vertice(TGrafo *g, char *nome, int idade) {
+    TGrafo *vertice = (TGrafo *) malloc(sizeof(TGrafo));
+    strcpy(vertice->nome, nome);
+    vertice->idade = idade;
+    vertice->prox = g;
+    vertice->prim_vizinho = NULL;
+    return vertice;
+}
+
+TGrafo *busca_vertice(TGrafo *vertice, char *nome) {
+    while ((vertice != NULL) && (strcmp(vertice->nome, nome) != 0)) {
+        vertice = vertice->prox;
+    }
+    return vertice;
+}
+
+TVizinho *busca_vizinho(TVizinho *vizinho, char *nome) {
+    while ((vizinho != NULL) && (strcmp(vizinho->nome, nome) != 0)) {
+        vizinho = vizinho->prox;
+    }
+    return vizinho;
+}
+
+void insere_aresta(TGrafo *g, char *nome_origem, char *nome_destino) {
+    TGrafo *pv1 = busca_vertice(g, nome_origem);
+    TGrafo *pv2 = busca_vertice(g, nome_destino);
+    if (pv1 != NULL && pv2 != NULL) {
+        TVizinho *vizinho = (TVizinho *) malloc(sizeof(TVizinho));
+        strcpy(vizinho->nome, nome_destino);
+        vizinho->prox = pv1->prim_vizinho;
+        pv1->prim_vizinho = vizinho;
+    }
+}
+
+void imprime(TGrafo *vertice) {
+    while (vertice != NULL) {
+        printf("Pessoa: %s - %d anos\n", vertice->nome, vertice->idade);
+        printf("Segue: ");
+        TVizinho *vizinho = vertice->prim_vizinho;
+        while (vizinho != NULL) {
+            printf("%s ", vizinho->nome);
+            vizinho = vizinho->prox;
+        }
+        printf("\n\n");
+        vertice = vertice->prox;
+    }
+}
+
+int numero_seguidos(TGrafo *g, char *nome) {
+    TVizinho *auxViz=NULL;
+    TGrafo *auxGraf=g;
+    int cont=0;
+
+    while(auxGraf!=NULL){
+        if(strcmp(auxGraf->nome, nome) ==0)
+            break;
+        else
+            auxGraf = auxGraf->prox;
+        
+    }
+    if(auxGraf!=NULL)
+        auxViz = auxGraf->prim_vizinho;
+    
+    while(auxViz!=NULL){
+        cont ++;
+        auxViz = auxViz->prox;
+    }
+
+
+    return cont;
+}
+
+int seguidores(TGrafo *g, char *nome, int imprime) {
+    TGrafo *auxG=g;
+    TVizinho *auxV=NULL;
+    int cont=0;
+
+    while(auxG){
+        if(strcmp(auxG->nome, nome)){
+            auxV = auxG->prim_vizinho;
+            while(auxV){
+                if(strcmp(auxV->nome, nome) == 0){
+                    cont ++;
+                    if(imprime)
+                        printf("%s ", auxG->nome);
+                }
+                auxV = auxV->prox;
+            }
+        }
+        auxG = auxG->prox;
+    }
+    return cont;
+}
+
+TGrafo *mais_popular(TGrafo *g) {
+    int maior=0, cont;
+    TGrafo *max=g, *auxG=g;
+
+    while(auxG){
+        cont = seguidores(g, auxG->nome, 0);
+        if(cont > maior){
+            maior = cont;
+            max = auxG;
+        }
+        auxG = auxG->prox;
+    }
+
+
+    return max;
+}
+
+int buscarIdade(TGrafo *g, char *nome){
+    TGrafo *aux=g;
+
+    while(aux){
+        if(strcmp(aux->nome, nome) == 0)
+            return aux->idade;
+        aux = aux->prox;
+    }
+    return 0;
+
+}
+
+int segue_mais_velho(TGrafo *g, int imprime) {
+    TGrafo *aux=g;
+    TVizinho *vizinho=NULL;
+
+    while(aux){
+        vizinho = aux->prim_vizinho;
+        while(vizinho){
+            if(aux->idade > buscarIdade(g, vizinho->nome))
+                break;
+            if(imprime)
+                printf("%s ", aux->nome);
+            vizinho = vizinho->prox;
+        }
+        aux=aux->prox;
+    }
+    printf("\n");
+    return 0;
+}
+
+void libera_vizinho(TVizinho *vizinho) {
+    if (vizinho != NULL) {
+        libera_vizinho(vizinho->prox);
+        free(vizinho);
+    }
+}
+
+void libera_vertice(TGrafo *vertice) {
+    if (vertice != NULL) {
+        libera_vizinho(vertice->prim_vizinho);
+        libera_vertice(vertice->prox);
+        free(vertice);
+    }
+}
+
+int main() {
+    /* A função main lê os dados de entrada, cria o grafo e chama as funções solicitadas no problema
+    * depois imprime os resultados solicitados
+    * ELA NÃO DEVE SER MODIFICADA
+    * */
+    int num_vertices, num_arestas;
+    char nome[30];
+    char origem[30], destino[30];
+    char l[100];
+    char delimitador[] = "-";
+    char *ptr;
+    int idade;
+    int i;
+    TGrafo *g = NULL;
+
+    //le numero de vertices
+    scanf("%d", &num_vertices);
+    //le e cria os vertices
+    for (i = 0; i < num_vertices; i++) {
+        scanf("%s", l);
+        //quebra a string de entrada
+        ptr = strtok(l, delimitador);
+        strcpy(nome,ptr);
+        //printf("%s", nome);
+        ptr = strtok(NULL, delimitador);
+        idade = atoi(ptr);
+        g = insere_vertice(g, nome, idade);
+    }
+
+    //Le numero de arestas e depois le os dados de cada aresta
+    //Cria as arestas no grafo
+    scanf("%d", &num_arestas);
+    for (i = 0; i < num_arestas; i++) {
+        scanf("%s", l);
+        //quebra a string de entrada
+        ptr = strtok(l, delimitador);
+        strcpy(origem,ptr);
+        //printf("%s", nome);
+        ptr = strtok(NULL, delimitador);
+        strcpy(destino,ptr);
+        insere_aresta(g, origem, destino);
+    }
+
+    //Le nome de pessoa
+    scanf("%s", nome);
+
+    //Encontra o número de seguidos dessa pessoa
+    printf("SEGUIDOS por %s: %d\n", nome, numero_seguidos(g, nome));
+
+
+    //Encontra os seguidores de uma determinada pessoa
+    printf("SEGUIDORES de %s:\n", nome);
+    seguidores(g, nome, 1);
+
+    TGrafo *p;
+
+    //Encontra mais popular
+    p = mais_popular(g);
+    printf("\nMAIS POPULAR: %s\n", p->nome);
+
+    //Encontra as pessoas que seguem apenas pessoas mais velhas
+    printf("SEGUEM APENAS PESSOAS MAIS VELHAS:\n");
+    segue_mais_velho(g,1);
+
+
+    libera_vertice(g);
+}
